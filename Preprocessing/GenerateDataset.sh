f5b8a74146af1c1a4be6cade6c941351afe4d83f
Kaixuan <kaixuanzhang2019@u.northwestern.edu>
2019-07-12 01:05:46 -0400

    change input file path

--- Preprocessing/GenerateDataset.sh
+++ Preprocessing/GenerateDataset.sh
@@ -6,11 +6,11 @@ outputpath='../../personnel-records/1954/trainset'
 
 rm $outputpath --recursive
 
-python3 DatasetGenerator.py --imgpath=$imgpath --labelfile='../../personnel-records/1954/csv/trainset_pr1954.csv' --labeldict='../../personnel-records/1954/csv/label_pr1954.csv' --outputpath=$outputpath
+python3 DatasetGenerator.py --imgpath=$imgpath --labelfile='../../personnel-records/1954/trainset_pr1954.csv' --labeldict='../../personnel-records/1954/Id2Name_label.csv' --outputpath=$outputpath
 
 outputpath='../../personnel-records/1954/testset'
 
 rm $outputpath --recursive
 
-python3 DatasetGenerator.py --imgpath=$imgpath --labelfile='../../personnel-records/1954/csv/testset_pr1954.csv' --labeldict='../../personnel-records/1954/csv/label_pr1954.csv' --outputpath=$outputpath
+python3 DatasetGenerator.py --imgpath=$imgpath --labelfile='../../personnel-records/1954/testset_pr1954.csv' --labeldict='../../personnel-records/1954/Id2Name_label.csv' --outputpath=$outputpath
 

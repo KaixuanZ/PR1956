2d6f832e69659c3ef0fb39f15222f5af96f40550
Kaixuan <kaixuanzhang2019@u.northwestern.edu>
2019-08-01 14:38:21 -0400

    Remove the advertisement pages in s3://personnel-records/1956/seg/page_rect

--- Preprocessing/Img2Page.sh
+++ Preprocessing/Img2Page.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-InputPath=${InputPath:-'../../data/'}
+InputPath=${InputPath:-'../../personnel-records/1956/scans/parsed/'}
 
-OutputPath=${OutputPath:-'../../output/'}
+OutputPath=${OutputPath:-'../../personnel-records/1956/seg/page_rect/'}
 
-rm $OutputPath --recursive
+#rm $OutputPath --recursive
 
 python3 Img2Page.py --inputpath=$InputPath --outputpath=$OutputPath 2>&1 | tee log_Img2Page.txt
+
+./RemoveAdPage.sh --Path=$OutputPath

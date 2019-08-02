f2ab475234c146480ac4c57bf68a1908777564ef
Triumph-Z <387937122@qq.com>
2019-08-02 14:20:08 -0400

    Delete MountNFSFileShare.sh

--- MountNFSFileShare.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-sudo mount -t nfs -o nolock,hard 18.144.66.40:/teikoku ../teikoku
-sudo mount -t nfs -o nolock,hard 18.144.66.40:/personnel-records ../personnel-records
-
-#umount
-##sudo umount ../teikoku
-##sudo umount ../personnel-records

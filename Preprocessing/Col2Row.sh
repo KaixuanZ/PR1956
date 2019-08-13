e6ca9fb1a77f4774f2546c4d6c91364ec11ae850
Kaixuan <kaixuanzhang2019@u.northwestern.edu>
2019-08-13 15:00:13 -0400

    code for col to row segmentation

---
+++ Preprocessing/Col2Row.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+ImgPath=${ImgPath:-'../../personnel-records/1956/scans/parsed'}
+
+ROIPath=${ROIPath:-'../../personnel-records/1956/seg/ROI'}
+
+OutputPath=${OutputPath:-'../../personnel-records/1956/seg/col_rect'}
+
+rm $OutputPath --recursive
+
+python3 Col2Row.py --imgdir=$ImgPath --ROIdir=$ROIPath --outputdir=$OutputPath 2>&1 | tee log_ROI.txt

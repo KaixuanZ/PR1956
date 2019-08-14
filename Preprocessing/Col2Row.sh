5cd3c8dc222c76b041c63eab5bf1564789c517b5
Kaixuan <kaixuanzhang2019@u.northwestern.edu>
2019-08-14 11:40:26 -0400

    change input for testing performance on PR1956

--- Preprocessing/Col2Row.sh
+++ Preprocessing/Col2Row.sh
@@ -2,10 +2,12 @@
 
 ImgPath=${ImgPath:-'../../personnel-records/1956/scans/parsed'}
 
-ROIPath=${ROIPath:-'../../personnel-records/1956/seg/ROI'}
+ColPath=${ROIPath:-'../../personnel-records/1956/seg/col_rect'}
 
-OutputPath=${OutputPath:-'../../personnel-records/1956/seg/col_rect'}
+OutputPath=${OutputPath:-'../../personnel-records/1956/tmp'}
+
+echo "removing $OutputPath"
 
 rm $OutputPath --recursive
 
-python3 Col2Row.py --imgdir=$ImgPath --ROIdir=$ROIPath --outputdir=$OutputPath 2>&1 | tee log_ROI.txt
+python3 Col2Row.py --imgdir=$ImgPath --coldir=$ColPath --outputdir=$OutputPath # 2>&1 | tee log_ROI.txt

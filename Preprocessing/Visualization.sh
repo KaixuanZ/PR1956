526b5e5667bd9c49207512d1062ffa65e2a6f82f
Kaixuan <kaixuanzhang2019@u.northwestern.edu>
2019-08-16 13:34:08 -0400

    change input/output file path

--- Preprocessing/Visualization.sh
+++ Preprocessing/Visualization.sh
@@ -2,9 +2,11 @@
 
 ImgDir=${ImgDir:-'../../personnel-records/1956/scans/parsed'}
 
-JsonDir=${JsonDir:-'../../personnel-records/1956/seg/col_rect'}
+JsonDir=${JsonDir:-'../../personnel-records/1956/sge/row_rect'}
 
 OutputDir=${OutputDir:-'../../personnel-records/1956/visualization'}
+
+echo "removing $OutputDir"
 
 rm -rf $OutputDir
 

32ef4604d854e40a9c69c5a10dcf88903cda8250
Kaixuan <kaixuanzhang2019@u.northwestern.edu>
2019-08-22 14:12:42 -0400

    finetune parameters and change input/output for test

--- Preprocessing/Visualization.py
+++ Preprocessing/Visualization.py
@@ -5,51 +5,7 @@ import os
 from joblib import Parallel, delayed
 import argparse
 import multiprocessing
-
 #read image and detected bounding box, output the image with bounding box
-
-def order_points(pts):
-    # sort the points based on their x-coordinates
-    xSorted = pts[np.argsort(pts[:, 0]), :]
-
-    # grab the left-most and right-most points from the sorted
-    # x-roodinate points
-    leftMost = xSorted[:2, :]
-    rightMost = xSorted[2:, :]
-
-    # now, sort the left-most coordinates according to their
-    # y-coordinates so we can grab the top-left and bottom-left
-    # points, respectively
-    leftMost = leftMost[np.argsort(leftMost[:, 1]), :]
-    (tl, bl) = leftMost
-
-    rightMost = rightMost[np.argsort(rightMost[:, 1]), :]
-    (tr, br) = rightMost
-
-    return np.array([tl, tr, br, bl])
-
-def CropRect(img, rect):
-    box = cv2.boxPoints(tuple(rect))
-    box = order_points(box)
-    # get width and height of the detected rectangle
-    if rect[2]<-45:
-        height,width = int(rect[1][0]),int(rect[1][1])
-    else:
-        width,height = int(rect[1][0]),int(rect[1][1])
-
-    src_pts = box.astype("float32")
-    # corrdinate of the points in box points after the rectangle has been straightened
-    dst_pts = np.array([[0, 0],
-                        [width, 0],
-                        [width, height],
-                        [0, height]], dtype="float32")
-
-    # the perspective transformation matrix
-    M = cv2.getPerspectiveTransform(src_pts, dst_pts)
-
-    # directly warp the rotated rectangle to get the straightened rectangle
-    warped = cv2.warpPerspective(img, M, (width, height))
-    return warped
 
 def TifFile(jsonfile):
     jsonfile = jsonfile.split('.')[0]
@@ -66,8 +22,8 @@ def main(jsonfile,jsondir,imgdir,outputdir):
     img = cv2.imread(os.path.join(imgdir, TifFile(jsonfile)))
     img = cv2.pyrDown(cv2.pyrDown(img))
     for file in files:
-        with open(os.path.join(jsondir,jsonfile,file)) as file:
-            rect = json.load(file)
+        with open(os.path.join(jsondir,jsonfile,file)) as rectjson:
+            rect = json.load(rectjson)
         box = cv2.boxPoints(tuple(rect))
         box = np.int0(box/scale)
         cv2.drawContours(img, [box], 0, (0,0,255), 1)
@@ -94,5 +50,5 @@ if __name__ == '__main__':
     imgdir=[args.imgdir] * len(rowRects)
     outputdir=[args.outputdir] * len(rowRects)
 
+    #Parallel(n_jobs=1)(map(delayed(main), rowRects, jsondir, imgdir, outputdir))
     Parallel(n_jobs=multiprocessing.cpu_count())(map(delayed(main), rowRects,jsondir,imgdir,outputdir))
-    #multiprocessing.cpu_count()-1

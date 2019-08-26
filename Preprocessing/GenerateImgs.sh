f4d48052c36f2a6d1c42a7a5b986a8171ffd73d1
Kaixuan <kaixuanzhang2019@u.northwestern.edu>
2019-08-26 11:54:59 -0400

    renamed from GeneColImg.py and GeneColImg.sh

--- Preprocessing/GenerateImgs.sh
+++ Preprocessing/GenerateImgs.sh
@@ -2,9 +2,9 @@
 
 ImgPath=${ImgPath:-'../../personnel-records/1956/scans/parsed'}
 
-ColPath=${ROIPath:-'../../personnel-records/1956/seg/col_rect'}
+RectPath=${RectPath:-'../../personnel-records/1956/seg/row_rect'}
 
-OutputPath=${OutputPath:-'../../personnel-records/1956/seg/col_img'}
+OutputPath=${OutputPath:-'../../personnel-records/1956/seg/row_img'}
 
 read -p "Do you want to remove previous output in $OutputPath? (y/n) " -n 1 -r
 echo -e "\n"
@@ -15,4 +15,4 @@ then
     rm $OutputPath --recursive
 fi
 
-python3 GeneColImg.py --imgdir=$ImgPath --coldir=$ColPath --outputdir=$OutputPath #2>&1 | tee log_Row.txt
+python3 GenerateImgs.py --imgdir=$ImgPath --rectdir=$RectPath --outputdir=$OutputPath #2>&1 | tee log_Row.txt

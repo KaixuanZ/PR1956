f05f223387842259c6e8bd7ad1347a410287869d
Kaixuan <kaixuanzhang2019@u.northwestern.edu>
2019-10-07 11:15:53 -0400

    code for visualize cls results

--- Visualization/RenderCls.py
+++ Visualization/RenderCls.py
@@ -3,11 +3,13 @@ import cv2
 import numpy as np
 import os
 import Rect
+import argparse
+from joblib import Parallel, delayed
 
 clean_names = lambda x: [i for i in x if i[0] != '.']
 
 #255-[b,g,r] because we want to output img with white background
-ColDict={"company":[255-0,255-0,255-255],  #red
+ColorDict={"company":[255-0,255-0,255-255],  #red
             "address":[255-0,255-128,255-0],    #green
             "variable":[255-32,255-165,255-218],        #golden rod
             "value":[255-209,255-206,255-0],    #dark turquoise
@@ -23,48 +25,72 @@ def Binarization(img,patchSize=9,threshold=7):
     img_b = cv2.adaptiveThreshold(img_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, patchSize, threshold)
     return img_b
 
-outputdir='visualization'
-jsondir='Row_rect'
-files=sorted(clean_names(os.listdir(jsondir)))
-scale = 2
-#import pdb;pdb.set_trace()
-img = cv2.imread('pr1956_f115_4.tif')
-img = cv2.pyrDown(img)
-img_b = Binarization(img)*255
+def GetImgFilename(jsonfile):
+    book, p, _ = jsonfile.split('.')[0].split('_')
+    p = p[0] + str(int(p[1:]))
+    return book + '_' + p + '.png'
 
-H,W = img_b.shape
-res = np.zeros([H,W,3], np.uint8)
-with open("cls.json") as clsjson:
-    cls = json.load(clsjson)
-cls=cls["name"]
+def main(ROIrect,args):
+    scale = 2
+    img = cv2.imread(os.path.join(args.imgdir,GetImgFilename(ROIrect)),0)
+    img = cv2.pyrDown(img)
+    img_b = Binarization(img)*255
 
-for i in range(58+59,58+59+74):
-    if i%20==0:
-        print(i)
-    with open(os.path.join(jsondir,files[i])) as rectjson:
-        rect = json.load(rectjson)
-    box = cv2.boxPoints(tuple(rect))
+    H,W = img_b.shape
+    res = np.zeros([H,W,3], np.uint8)
+
+
+    with open(os.path.join(args.clsdir,ROIrect)) as clsjson:
+        cls = json.load(clsjson)
+    cls=cls["name"]
+
+    with open(os.path.join(args.rowrectdir, ROIrect)) as rectjson:
+        rowrects = json.load(rectjson)
+    tmp=[]
+    for key in rowrects.keys():
+        tmp+=rowrects[key]
+    rowrects=tmp
+
+    with open(os.path.join(args.colrectdir, ROIrect)) as rectjson:
+        ROIrect = json.load(rectjson)
+
+    for i in range(len(rowrects)):
+
+        box = cv2.boxPoints(tuple(rowrects[i]))
+        box = np.int0(box/scale)
+        mask = np.zeros([H,W,3], np.uint8)
+
+        cv2.drawContours(mask, [box], -1, ColorDict[cls[i]], -1)
+        b,g,r = mask[:,:,0],mask[:,:,1],mask[:,:,2]
+        #import pdb;pdb.set_trace()
+        b,g,r = b*img_b,g*img_b,r*img_b
+
+        mask = cv2.merge([b,g,r])
+        #import pdb;pdb.set_trace()
+
+
+        res+=mask
+
+        res=255-res
+
+    box = cv2.boxPoints(tuple(ROIrect))
     box = np.int0(box/scale)
-    mask = np.zeros([H,W,3], np.uint8)
+    ROI_rect = cv2.minAreaRect(box)
+    warped,_=Rect.CropRect(res,ROI_rect)
 
-    cv2.drawContours(mask, [box], -1, ColDict[cls[i]], -1)
-    b,g,r = mask[:,:,0],mask[:,:,1],mask[:,:,2]
-    #import pdb;pdb.set_trace()
-    b,g,r = b*img_b,g*img_b,r*img_b
+    cv2.imwrite(os.path.join(args.outputdir,ROIrect.split('.')[0]+'.png'),warped)
 
-    mask = cv2.merge([b,g,r])
-    #import pdb;pdb.set_trace()
+if __name__ == '__main__':
+    # construct the argument parse and parse the arguments
+    parser = argparse.ArgumentParser(description='Visualize classification results')
+    parser.add_argument( '--imgdir', type=str)
+    parser.add_argument( '--outputpath', type=str)
+    parser.add_argument( '--pagerectdir', type=str)
+    parser.add_argument( '--rowrectdir', type=str)
+    parser.add_argument( '--clsdir', type=str)
 
+    args = parser.parse_args()
+    clean_names = lambda x: [i for i in x if i[0] != '.']
+    ROIrects=sorted(clean_names(os.listdir(args.pagerectdir)))
 
-    res+=mask
-
-res=255-res
-
-with open('Col_rect/pr1956_f0115_4_0_2.json') as rectjson:
-    ROI_rect = json.load(rectjson)
-box = cv2.boxPoints(tuple(ROI_rect))
-box = np.int0(box/scale)
-ROI_rect = cv2.minAreaRect(box)
-warped,_=Rect.CropRect(res,ROI_rect)
-
-cv2.imwrite(os.path.join(outputdir,'Cls.png'),warped)
+    Parallel(n_jobs=-1)(map(delayed(main), ROIrects, [args] * len(ROIrects)))

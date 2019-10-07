b59deb8e243d1f649d3772380542ff5c3373de1e
Kaixuan <kaixuanzhang2019@u.northwestern.edu>
2019-10-07 11:16:41 -0400

    change input from PR1956 to PR1954

--- Preprocessing/GenerateDataset.sh
+++ Preprocessing/GenerateDataset.sh
@@ -1,23 +1,8 @@
 #!/usr/bin/env bash
 
-imgpath='../../personnel-records/1956/seg/row_img/'
+imgpath='../../results/personnel-records/1954/seg/firm/row_img'
 
-outputpath='../../personnel-records/1956/trainset'
-
-read -p "Do you want to remove previous output in $outputpath? (y/n) " -n 1 -r
-echo -e "\n"
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    echo "removing $outputpath"
-
-    rm $outputpath --recursive
-fi
-
-
-python3 DatasetGenerator.py --imgpath=$imgpath --labelfile='../../personnel-records/1956/csv/trainset_pr1956.csv' --labeldict='../../personnel-records/1956/csv/Id2Name_label.csv' --outputpath=$outputpath
-
-
-outputpath='../../personnel-records/1956/testset'
+outputpath='../../results/personnel-records/1954/trainset'
 
 read -p "Do you want to remove previous output in $outputpath? (y/n) " -n 1 -r
 echo -e "\n"
@@ -28,5 +13,20 @@ then
     rm $outputpath --recursive
 fi
 
-python3 DatasetGenerator.py --imgpath=$imgpath --labelfile='../../personnel-records/1956/csv/testset_pr1956.csv' --labeldict='../../personnel-records/1956/csv/Id2Name_label.csv' --outputpath=$outputpath
+
+python3 DatasetGenerator.py --imgpath=$imgpath --labelfile='../../results/personnel-records/1954/labeled_data/trainset_pr1954.csv' --labeldict='../../results/personnel-records/1954/labeled_data/Id2Name_label.csv' --outputpath=$outputpath
+
+
+outputpath='../../results/personnel-records/1954/testset'
+
+read -p "Do you want to remove previous output in $outputpath? (y/n) " -n 1 -r
+echo -e "\n"
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo "removing $outputpath"
+
+    rm $outputpath --recursive
+fi
+
+python3 DatasetGenerator.py --imgpath=$imgpath --labelfile='../../results/personnel-records/1954/labeled_data/testset_pr1954.csv' --labeldict='../../results/personnel-records/1954/labeled_data/Id2Name_label.csv' --outputpath=$outputpath
 

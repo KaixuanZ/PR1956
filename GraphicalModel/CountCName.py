c3e417c6811d19e40268cd78d44b90638db15dbd
Kaixuan <kaixuanzhang2019@u.northwestern.edu>
2019-10-18 21:58:57 -0400

    change input from PR1954 to PR1956

--- GraphicalModel/CountCName.py
+++ GraphicalModel/CountCName.py
@@ -4,17 +4,16 @@ import json
 import numpy as np
 
 #in the cls json file, 2 represent company name
-inputpath='../../results/personnel-records/1954/cls/firm/'
+inputpath='../../results/personnel-records/1956/cls/CRF/supplement/'
 clean_names = lambda x: [i for i in x if i[0] != '.']
 count=0
 for dir in sorted(clean_names(os.listdir(inputpath))):  #one dir includes prob for one page
     print("processing "+dir)
     with open(os.path.join(inputpath,dir)) as jsonfile:
         cls = json.load(jsonfile)
-    cls=cls['id']
     for i in range(len(cls)):
-        if i==0 and cls[i]==1:
+        if i==0 and cls[i]=='company':
             count+=1
-        elif cls[i]==1 and cls[max(i-1,0)]!=1:
+        elif cls[i]=='company' and cls[max(i-1,0)]!='company':
             count+=1
 print(count)

528739176d837f15d7734449a65c7ef28fd374d8
Kaixuan <kaixuanzhang2019@u.northwestern.edu>
2019-10-21 23:56:53 -0400

    1. rename functions 2. change CropRect() a little bit, according to how opencv define [height,width] of rect and [height,width] of matrix

--- Rect.py
+++ Rect.py
@@ -23,20 +23,19 @@ def DistOfRects(rect1,rect2):
     c1,c2=np.array(rect1[0]),np.array(rect2[0])
     return np.sum((c1-c2)**2)**0.5
 
+def RectOnDstImg(box, M_src2dst):
+    # Given box on src img, and transformation M from src to dst, return the box on dst img.
+    pts=PtsOnDstImg(cv2.boxPoints(tuple(box)), M_src2dst)
+    return cv2.minAreaRect(pts)
 
-def RectOnSrcImg(box, M):
-    # Given box on dst img, and transformation M from src to dst, return the box on src img.
-    box=PtsOnSrcImg(box, M)
-    return cv2.minAreaRect(box)
-
-def PtsOnSrcImg(pts, M):
-    #Given pts on dst img, and transformation M from src to dst, return the pts on src img.
+def PtsOnDstImg(pts, M_src2dst):
+    #Given pts on src img, and transformation M from src to dst, return the pts on dst img.
     pts = np.array(pts)
     pts = np.concatenate((pts, np.ones([pts.shape[0], 1])), axis=1)
-    M_inv = np.linalg.inv(M)
-    # box on the original img
-    pts = np.dot(M_inv[0:2, :], pts.T).T
-    pts = np.int0(pts + 0.5)
+    # box on the dst img
+    pts = np.dot(M_src2dst, pts.T).T
+    pts = pts / pts[:, 2, None]
+    pts = np.int0(pts[:,0:2]+0.5)
     return OrderPoints(pts)
 
 def OrderPoints(pts):
@@ -62,7 +61,7 @@ def OrderPoints(pts):
 
 def CropRect(img, rect):
     #crop img w.r.t. rect, return the warped img and the transformation M (from src to dst)
-    box = cv2.boxPoints(tuple(rect))
+    box = cv2.boxPoints(tuple(rect))    # e.g. [[0,0],[0,1],[1,1],[1,0]] <==> ((0.5, 0.5), (1.0, 1.0), -90.0)
     box = OrderPoints(box)
     # get width and height of the detected rectangle
     if rect[2] < -45:
@@ -73,13 +72,14 @@ def CropRect(img, rect):
     src_pts = box.astype("float32")
     # corrdinate of the points in box points after the rectangle has been straightened
     dst_pts = np.array([[0, 0],
-                        [width-1, 0],
-                        [width-1, height-1],
-                        [0, height-1]], dtype="float32")
+                        [width, 0],
+                        [width, height],
+                        [0, height]], dtype="float32")
 
     # the perspective transformation matrix
     M = cv2.getPerspectiveTransform(src_pts, dst_pts)
 
     # directly warp the rotated rectangle to get the straightened rectangle
-    warped = cv2.warpPerspective(img, M, (width, height))
+    warped = cv2.warpPerspective(img, M, (width+1, height+1))
+    #import pdb;pdb.set_trace()
     return warped, M

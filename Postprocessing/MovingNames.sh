d7e206ea8c06b03c12003bc7864dff47ed4546b0
Kaixuan <kaixuanzhang2019@u.northwestern.edu>
2019-11-01 15:27:58 -0400

    change output file directory name

--- Postprocessing/MovingNames.sh
+++ Postprocessing/MovingNames.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-declare -a Sections=("firm" "supplement")
+declare -a Sections=( "bank" "credit_union" "official_office" )
+
+#declare -a Sections=("firm" "supplement")
 
 InputPath=${InputPath:-'../../results/personnel-records/1956/seg/'}
 
 ClsPath=${ClsPath:-'../../results/personnel-records/1956/cls/CRF/'}
 
-read -p "Do you want to remove previous output in $InputPath/section/col_img_modified? (y/n) " -n 1 -r
+read -p "Do you want to remove previous output in $InputPath/section/col_img_opt_for_name? (y/n) " -n 1 -r
 echo -e "\n"
 DELETE="N"
 if [[ $REPLY =~ ^[Yy]$ ]]
@@ -20,7 +22,7 @@ for section in "${Sections[@]}"; do
     ClsPath_section="$ClsPath$section"
     ColRectPath_section="$InputPath$section/col_rect"
     RowRectPath_section="$InputPath$section/row_rect"
-    OutputPath_section="$InputPath$section/col_img_modified"
+    OutputPath_section="$InputPath$section/col_img_opt_for_name"
 
     if test "$DELETE" == 'Y'
     then

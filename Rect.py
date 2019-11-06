bfd8269cc6b18df61773e1ed414ea06f0da00a64
Kaixuan <kaixuanzhang2019@u.northwestern.edu>
2019-11-06 14:35:01 -0500

    update code for rect

--- Rect.py
+++ Rect.py
@@ -7,37 +7,42 @@ from shapely.geometry import Polygon
 def AreaOfOverlap(rect1,rect2,rect=True):
     #return the size of intersection area of rect1 and rect2
     if rect:
-        rect1 = Polygon(cv2.boxPoints(tuple(rect1)))
-        rect2 = Polygon(cv2.boxPoints(tuple(rect2)))
-    return rect1.intersection(rect2).area
+        plg1 = Polygon(cv2.boxPoints(tuple(rect1)))
+        plg2 = Polygon(cv2.boxPoints(tuple(rect2)))
+    return plg1.intersection(plg2).area
 
 def CombineRects(rect1,rect2):
     #return a minAreaRect which contains rect1 and rect2
-    box1=cv2.boxPoints(tuple(rect1))
-    box2=cv2.boxPoints(tuple(rect2))
-    box=np.concatenate((box1,box2),axis=0)
-    return cv2.minAreaRect(box)
+    pts1=cv2.boxPoints(tuple(rect1))
+    pts2=cv2.boxPoints(tuple(rect2))
+    pts=np.concatenate((pts1,pts2),axis=0)
+    return cv2.minAreaRect(pts)
 
 def DistOfRects(rect1,rect2):
     #return the L2 distance of centers of two rects
     c1,c2=np.array(rect1[0]),np.array(rect2[0])
     return np.sum((c1-c2)**2)**0.5
 
+def RectOnDstImg(rect, M_src2dst,flag_box=False):
+    # Given rect on src img, and transformation M from src to dst, return the rect on dst img.
+    if flag_box:
+        pts=PtsOnDstImg(rect,M_src2dst)
+    else:
+        pts=PtsOnDstImg(cv2.boxPoints(tuple(rect)), M_src2dst)
+    return cv2.minAreaRect(pts)
 
-def RectOnSrcImg(box, M):
-    # Given box on dst img, and transformation M from src to dst, return the box on src img.
-    box=PtsOnSrcImg(box, M)
-    return cv2.minAreaRect(box)
+def PtsOnDstImg(pts, M_src2dst, orderPts=True):
+    #Given pts on src img, and transformation M from src to dst, return the pts on dst img.
 
-def PtsOnSrcImg(pts, M):
-    #Given pts on dst img, and transformation M from src to dst, return the pts on src img.
     pts = np.array(pts)
     pts = np.concatenate((pts, np.ones([pts.shape[0], 1])), axis=1)
-    M_inv = np.linalg.inv(M)
-    # box on the original img
-    pts = np.dot(M_inv[0:2, :], pts.T).T
-    pts = np.int0(pts + 0.5)
-    return OrderPoints(pts)
+    # pts on the dst img
+    pts = np.dot(M_src2dst, pts.T).T
+    pts = pts / pts[:, 2, None]
+    pts = np.int0(pts[:,0:2]+0.5)
+    if orderPts:
+        return OrderPoints(pts)
+    return pts
 
 def OrderPoints(pts):
     # sort the points based on their x-coordinates
@@ -62,24 +67,24 @@ def OrderPoints(pts):
 
 def CropRect(img, rect):
     #crop img w.r.t. rect, return the warped img and the transformation M (from src to dst)
-    box = cv2.boxPoints(tuple(rect))
-    box = OrderPoints(box)
+    pts = cv2.boxPoints(tuple(rect))    # e.g. [[0,0],[0,1],[1,1],[1,0]] <==> ((0.5, 0.5), (1.0, 1.0), -90.0)
+    pts = OrderPoints(pts)
     # get width and height of the detected rectangle
     if rect[2] < -45:
         height, width = int(rect[1][0]), int(rect[1][1])
     else:
         width, height = int(rect[1][0]), int(rect[1][1])
 
-    src_pts = box.astype("float32")
+    src_pts = pts.astype("float32")
     # corrdinate of the points in box points after the rectangle has been straightened
     dst_pts = np.array([[0, 0],
-                        [width-1, 0],
-                        [width-1, height-1],
-                        [0, height-1]], dtype="float32")
+                        [width, 0],
+                        [width, height],
+                        [0, height]], dtype="float32")
 
     # the perspective transformation matrix
     M = cv2.getPerspectiveTransform(src_pts, dst_pts)
 
     # directly warp the rotated rectangle to get the straightened rectangle
-    warped = cv2.warpPerspective(img, M, (width, height))
+    warped = cv2.warpPerspective(img, M, (width+1, height+1))
     return warped, M

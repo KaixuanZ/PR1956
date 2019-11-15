12ad79c418a122e5c0d3daa31c8213f882df239e
Kaixuan <kaixuanzhang2019@u.northwestern.edu>
2019-11-15 14:40:06 -0500

    use sync instead of cp

--- DownloadPR1956.sh
+++ DownloadPR1956.sh
@@ -10,9 +10,9 @@ mkdir $ResDir
 #download all orginal images from AWS S3
 AWS_S3_Path='s3://harvardaha-raw-data/personnel-records/1956/'
 
-aws2 s3 cp $AWS_S3_Path $ImgDir
+aws2 s3 sync $AWS_S3_Path $ImgDir
 
 #download all segmentation results from AWS S3
 AWS_S3_Path='s3://harvardaha-results/personnel-records/1956/'
-aws2 s3 cp $AWS_S3_Path $ResDir
+aws2 s3 sync $AWS_S3_Path $ResDir
 

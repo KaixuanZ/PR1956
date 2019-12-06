1f09ab831a319d6d53dcd7d6ceb7fb5f1019b6e4
Kaixuan <kaixuanzhang2019@u.northwestern.edu>
2019-12-06 12:26:11 -0500

    change input from PR1956 to PR1954

--- Preprocessing/ROI2Col.sh
+++ Preprocessing/ROI2Col.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-ImgPath=${ImgPath:-'../../personnel-records/1956/scans/parsed'}
+ImgPath=${ImgPath:-'../../personnel-records/1954/scans/index'}
 
-ROIPath=${ROIPath:-'../../personnel-records/1956/seg/ROI'}
+ROIPath=${ROIPath:-'../../personnel-records/1954/seg/ROI'}
 
-OutputPath=${OutputPath:-'../../personnel-records/1956/seg/col_rect'}
+OutputPath=${OutputPath:-'../../personnel-records/1954/seg/col_rect'}
 
 read -p "Do you want to remove previous output in $OutputPath? (y/n) " -n 1 -r
 echo -e "\n"
